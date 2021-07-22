63c9c8a3506cd38937d63e4804d2d45852f307b2
danielzhang823 <86642973+danielzhang823@users.noreply.github.com>
2021-07-22 18:18:05 -0400

    Update joy_teleop.py

--- scripts/joy_teleop.py
+++ scripts/joy_teleop.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import importlib
 from threading import Thread
-
 from tf.transformations import quaternion_from_euler 
 import actionlib
 import genpy.message
@@ -22,8 +21,8 @@ from geometry_msgs.msg import (
     PoseWithCovarianceStamped, 
     Quaternion, 
 ) 
-
-#from mushr_cvae.py import *
+#cvae imports
+from mushr_cvae import *
 
 button_states = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 joy_states = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
@@ -37,11 +36,19 @@ const_throttle = 1
 vel_scale = 0.5
 turn_scale = 0.34
 
+net = cVAE()
+
+app_folder = "/Users/ceronj22/Desktop/"
+name = "mushr_cvae_weights"
+
+checkpoint = torch.load(str(app_folder + name))
+net.load_state_dict(checkpoint['model_state_dict'])
+
+
 def joy_callback(data):
     button_states[:] = data.buttons[:]
     joy_states[:] = data.axes
     
-
 def motor_speed(data):
     motor_speed.append(data.data)
 
@@ -55,37 +62,33 @@ def pf_callback(data):
     x_pf = data.pose.position.x
     y_pf = data.pose.position.y
     z_orien_pf = data.pose.orientation.z
-    s = (x_pf,y_pf,z_orien_pf)
-
+    s = (x_pf, y_pf, z_orien_pf)
    
-
-#def cvae_action(z, state):
-#    cvae_action = motor_speed, wheel_angle
-#    return(cvae_action)
-
 def send_command(pub_controls):
-    #drive = AckermannDrive(steering_angle= cvae_action[1], speed= const_throttle * (cvae_action[0]))  
+    #drive = AckermannDrive(steering_angle= turn_scale * cvae_output[1], speed = vel_scale * (-joy_states[3] * cvae_output[0]))
+    #pub_controls.publish(AckermannDriveStamped(drive=drive))   
+    
     drive = AckermannDrive(steering_angle= turn_scale * joy_states[0], speed = vel_scale * (-joy_states[3] * joy_states[1]))
     pub_controls.publish(AckermannDriveStamped(drive=drive))  
-def conv_driver_space():
+def conv_to_driver_space():
     #mutate pf space to driver space
     s[0] = -s[0]
     s[2] =  s[2] + 1/2
 
-
 def timer_callback(data):
-    #conv_driver_space()
-    #joy_to_latent_map(constant_z)
-    #cvae_action(constant_latent, s)
+    conv_driver_space()
+    joy_to_latent()
+    cvae_output = cvae_to_action(net.P(constant_latent, s))
     send_command(pub_controls)
     
 def publisher():
     rospy.init_node("publisher", anonymous=True)
     rospy.Subscriber("/car/teleop/joy", sensor_msgs.msg.Joy, joy_callback)
+    # particle filter
     #rospy.Subscriber("/car/particle_filter/inferred_pose", geometry_msgs.msg.PoseStamped, pf_callback)
-    rospy.Subscriber("/car/car_pose", geometry_msgs.msg.PoseStamped, pf_callback)
+    # odometry pos
+    #rospy.Subscriber("/car/car_pose", geometry_msgs.msg.PoseStamped, pf_callback)
     rospy.Timer(rospy.Duration(1.0 / hertz), timer_callback)
-    print(joy_states)
     rospy.spin()
 
     
