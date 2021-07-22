28e9b16303d023e84c8432dade0a1b36be9219d5
danielzhang823 <86642973+danielzhang823@users.noreply.github.com>
2021-07-22 12:41:29 -0400

    Add files via upload

--- scripts/publisherwip.py
+++ scripts/publisherwip.py
@@ -1,7 +1,8 @@
-\#!/usr/bin/env python
+#!/usr/bin/env python
 import importlib
 from threading import Thread
 
+from tf.transformations import quaternion_from_euler 
 import actionlib
 import genpy.message
 import numpy as np
@@ -10,11 +11,19 @@ import rosservice
 import rostopic
 from rospy import ROSException
 from rosservice import ROSServiceException
-from stf_msgs.msg import Float64, Bool
-import sensor msgs.msg
+from std_msgs.msg import Float64, Bool
+import sensor_msgs.msg
 import geometry_msgs.msg
+from ackermann_msgs.msg import AckermannDrive, AckermannDriveStamped 
+from geometry_msgs.msg import ( 
+    Point, 
+    Pose, 
+    PoseWithCovariance, 
+    PoseWithCovarianceStamped, 
+    Quaternion, 
+) 
 
-from cvae.py import *
+#from mushr_cvae.py import *
 
 button_states = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 joy_states = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
@@ -39,7 +48,7 @@ def wheel_angle_callback(data):
     wheel_angle.append(data.data)
 
 def joy_to_latent():
-    latent_z = (joy_states[0], latent_y = joy_states[1])
+    latent_z = (joy_states[0], joy_states[1])
 
 def pf_callback(data):
     x_pf = data.pose.position.x
@@ -47,33 +56,50 @@ def pf_callback(data):
     z_orien_pf = data.pose.orientation.z
     s = (x_pf,y_pf,z_orien_pf)
 
-def cvae_action(z, state):
-    cvae_action = motor_speed, wheel_angle
-    return(cvae_action)
+   
 
-def send_command()
-    drive = AckermanDrive(steering_angle= (cvae_action[0]), speed= const_throttle * (cvae_action[1]))   
+#def cvae_action(z, state):
+#    cvae_action = motor_speed, wheel_angle
+#    return(cvae_action)
+
+def send_command(pub_controls):
+    #drive = AckermannDrive(steering_angle= cvae_action[1], speed= const_throttle * (cvae_action[0]))  
+    drive = AckermannDrive(steering_angle= joy_states[0], speed = joy_states[1])
+    pub_controls.publish(AckermannDriveStamped(drive=drive))  
 
 def conv_driver_space():
     #mutate pf space to driver space
     s[0] = -s[0]
     s[2] =  s[2] + 1/2
 
-def timer_callback():
-    rospy.init_node("listener", anonymous=True)
-    rospy.Timer(rospy.Duration(1.0 / hertz, timer_callback)
-    rospy.Subscriber("/car/teleop/joy", sensor_msgs.Joy, joy_callback)
-    rospy.Subscriber("/car/particle_filter/inferred_pose", geometry_msgs.msg.PoseStamped, pf_callback)
-    conv_driver_space()
-    joy_to_latent_map(constant_z)
-    cvae_action(constant_latent, s)
-    send_command()
+
+def timer_callback(data):
+    #conv_driver_space()
+    #joy_to_latent_map(constant_z)
+    #cvae_action(constant_latent, s)
+    send_command(pub_controls)
+    
+def publisher():
+    rospy.init_node("publisher", anonymous=True)
+    rospy.Subscriber("/car/teleop/joy", sensor_msgs.msg.Joy, joy_callback)
+    #rospy.Subscriber("/car/particle_filter/inferred_pose", geometry_msgs.msg.PoseStamped, pf_callback)
+    rospy.Subscriber("/car/car_pose", geometry_msgs.msg.PoseStamped, pf_callback)
+    rospy.Timer(rospy.Duration(1.0 / hertz), timer_callback)
     rospy.spin()
+
     
 if __name__ == "__main__": 
-    rospy.init_node("publisher") 
-    print("Running CVAE Assisted Teleop!)
-    timer_callback()
+    print("Running CVAE Assisted Teleop!")
+
+    control_topic = rospy.get_param("~control_topic", "/car/mux/ackermann_cmd_mux/input/teleop") 
+    pub_controls = rospy.Publisher(control_topic, AckermannDriveStamped, queue_size=1) 
+ 
+    init_pose_topic = rospy.get_param("~init_pose_topic", "/initialpose") 
+    pub_init_pose = rospy.Publisher(init_pose_topic, PoseWithCovarianceStamped, queue_size=1) 
+ 
+
+ 
+    publisher()
     print("Quit CVAE Assisted Teleop")
  
     
